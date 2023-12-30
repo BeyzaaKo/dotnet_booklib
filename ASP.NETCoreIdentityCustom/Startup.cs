dfc3f3b0d2396665a9df91eaa4f2e05a8a4f646c
Beyza Kocamış <beeeyza.ko@gmail.com>
2023-12-30 15:25:23 +0300

    dotnet booklib

---
+++ ASP.NETCoreIdentityCustom/Startup.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ASP.NETCoreIdentityCustom.Areas.Identity.Data;
+using ASP.NETCoreIdentityCustom.Core;
+using ASP.NETCoreIdentityCustom.Core.Repositories;
+using ASP.NETCoreIdentityCustom.Repositories;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connectionString = Configuration.GetConnectionString("ApplicationDbContextConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddControllersWithViews();
+
+        services.AddHttpClient();
+
+        services.AddScoped<BookRepository>();
+
+        AddAuthorizationPolicies(services);
+        AddScopedServices(services);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
+    }
+
+    private void AddAuthorizationPolicies(IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("EmployeeOnly", policy => policy.RequireClaim("EmployeeNumber"));
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(Constants.Policies.RequireAdmin, policy => policy.RequireRole(Constants.Roles.Administrator));
+            options.AddPolicy(Constants.Policies.RequireManager, policy => policy.RequireRole(Constants.Roles.Manager));
+        });
+    }
+
+    private void AddScopedServices(IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<BookRepository>();
+    }
+}
