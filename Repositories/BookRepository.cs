4427ff9af88bc8536eb8ece11fe82cda3e95e097
Beyza Kocamış <110380998+BeyzaaKo@users.noreply.github.com>
2024-01-10 04:12:18 +0300

    Add files via upload

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using ASP.NETCoreIdentityCustom.Models;
+
+namespace ASP.NETCoreIdentityCustom.Repositories
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title == title && x.Author == authorName).ToList();
+        }
+
+        //böyle elle almak istemiyorum, api ile çekmek veya database oluşturmak istiyorum
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { Id = 1, Title = "Normal People", Author = "Sally Rooney", Description="This section will be description of book."},
+                new BookModel() { Id = 2, Title = "The Midnight Library", Author = "Matt Haig", Description="This section will be description of book."},
+                new BookModel() { Id = 3, Title = "Atomic Habits", Author = "James Clear", Description="This section will be description of book."},
+                new BookModel() { Id = 4, Title = "Animal Farm", Author = "George Orwell", Description="This section will be description of book."},
+                new BookModel() { Id = 5, Title = "The Secret History", Author = "Donna Tartt", Description="This section will be description of book."},
+            };
+        }
+    }
+}
