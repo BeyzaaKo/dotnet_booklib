50bec5b03f81c3e64ced7a08b6a27367a319f63a
Beyza Kocamış <110380998+BeyzaaKo@users.noreply.github.com>
2024-01-10 04:02:46 +0300

    Add files via upload

---
+++ Controllers/RoleViewsController.cs
@@ -0,0 +1,29 @@
+﻿using ASP.NETCoreIdentityCustom.Core;
+using ASP.NETCoreIdentityCustom.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreIdentityCustom.Controllers
+{
+    public class RoleViewsController : Controller
+    {
+        [Authorize]
+        public IActionResult Books()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Discover()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NETCoreIdentityCustom.Models
+{
+    public class UserProfile
+    {
+    public string? UserName { get; set; }
+    public string? FullName { get; set; }
+    public string? Biography { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+    }
+}
